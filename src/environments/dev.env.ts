704ae4db49f0829ee2579d48d34af17b60be2223
Shani-Kumar-Gupta <shani.gupta@geminisolutions.in>
2021-01-26 20:54:17 +0530

    created express server | connected to mongodb | basic routing done | basic middleware done | handling env

---
+++ src/environments/dev.env.ts
@@ -0,0 +1,5 @@
+import { Environment } from "./env";
+
+export const devEnvironment: Environment = {
+    db_url : 'mongodb+srv://shanidb:171999Shani@mongodb.1fqkw.mongodb.net/test?retryWrites=true&w=majority'
+}
